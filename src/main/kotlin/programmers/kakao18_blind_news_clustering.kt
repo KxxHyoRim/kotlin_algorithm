7b13b74e89e5ff2ca49b952483d274650f759d28
kxxhyorim <rlagyfla0709@gmail.com>
2024-02-27 17:47:41 +0900

    [refactor] window, all 확장함수 사용 / [1차] 뉴스 클러스터링 (2018 KAKAO BLIND RECRUITMENT)

--- src/main/kotlin/programmers/kakao18_blind_news_clustering.kt
+++ src/main/kotlin/programmers/kakao18_blind_news_clustering.kt
@@ -1,6 +1,5 @@
 package programmers
 
-import java.lang.Math.floor
 import kotlin.math.max
 import kotlin.math.min
 
@@ -15,23 +14,10 @@ fun String.toMultiSet(): List<String> {
 }
 
 fun solution(str1: String, str2: String): Int {
-    var list1 = mutableListOf<String>()
-    var list2 = mutableListOf<String>()
-
-    str1.lowercase().fold("") { a, b -> list1.add("$a$b");"$b" }
-    list1 = list1.drop(1) // 처음 원소는 길이가 1이다
-        .filter { it[0].isLetter() && it[1].isLetter() }
-        .toMutableList()
-
-
-    str2.lowercase().fold("") { a, b -> list2.add("$a$b");"$b" }
-
-    list2 = list2.drop(1) // 처음 원소는 길이가 1이다
-        .filter { it[0].isLetter() && it[1].isLetter() }
-        .toMutableList()
+    val list1 = str1.toMultiSet()
+    val list2 = str2.toMultiSet()
 
     if (list1.isEmpty() && list2.isEmpty()) return 65536
-
 
     val map1 = list1.groupingBy { it }.eachCount().toSortedMap()
     val map2 = list2.groupingBy { it }.eachCount().toSortedMap()
@@ -40,14 +26,13 @@ fun solution(str1: String, str2: String): Int {
     var intersect = 0
     var union = 0
 
-
     for (key in keys) {
         intersect += min(map1[key] ?: 0, map2[key] ?: 0)
         union += max(map1[key] ?: 0, map2[key] ?: 0)
     }
 
     val ratio: Double = intersect.toDouble() / union
-    val answer = floor(ratio * 65536).toInt()
+    val answer = (ratio * 65536).toInt() // toInt()는 내림 적용
 
     println(answer)
 
