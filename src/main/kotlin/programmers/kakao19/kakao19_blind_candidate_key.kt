91661e97380c08485d7109618c428d6724d29613
kxxhyorim <rlagyfla0709@gmail.com>
2024-04-02 14:54:40 +0900

    후보키 (kakao 2019 Blind)

---
+++ src/main/kotlin/programmers/kakao19/kakao19_blind_candidate_key.kt
@@ -0,0 +1,72 @@
+package programmers.kakao19
+
+import kotlin.math.pow
+
+class Candidate {
+    fun solution(relation: Array<Array<String>>): Int {
+
+        val row = relation.size
+        val column = relation[0].size
+        val combinationNum = 2.0.pow(column).toInt()
+
+        // 후보키 목록 뽑은 다음 size가 작은것부터 정렬
+        val sortedNumbers = (1 until combinationNum)
+            .sortedBy { Integer.bitCount(it) }
+
+        val candidates = mutableListOf<String>()
+
+        for (number in sortedNumbers) {
+            val binary = number.toString(2).padStart(column, '0') // 2진수 변환 string
+
+            // 최소성 검사
+            var isPossible = true
+            for (candidate in candidates) {
+                // println("${binary.toInt(2)} ${candidate.toInt(2)}")
+                // println("and : ${binary.toInt(2) and candidate.toInt(2)}")
+                // println("cdd : ${candidate.toInt(2)}")
+                // println()
+                if ((binary.toInt(2) and candidate.toInt(2)) == candidate.toInt(2)) {
+                    isPossible = false
+                    break
+                }
+            }
+            if (!isPossible) continue
+
+
+            val set = hashSetOf<String>() // 유일성 검사
+
+            for (i in 0 until row) { // 가로 한줄씩 검사
+                val r = relation[i]
+                var string = ""
+                binary.mapIndexed { idx, it ->
+                    if (it == '1') string += r[idx]
+                }
+                if (set.contains(string)) {
+                    break
+                } else {
+                    set.add(string)
+                }
+            }
+            if (set.size == row) {
+                println(binary)
+                candidates.add(binary)
+            }
+        }
+        println(candidates.size)
+        return candidates.size
+    }
+}
+
+fun main() {
+
+    val relation =
+        arrayOf(
+            arrayOf("100", "ryan", "music", "2", "0"),
+            arrayOf("200", "apeach", "math", "2", "1"),
+            arrayOf("300", "tube", "computer", "3", "2"),
+            arrayOf("400", "con", "computer", "4", "3"),
+            arrayOf("500", "muzi", "music", "3", "4"),
+            arrayOf("600", "apeach", "music", "2", "5")
+        )
+    Candidate().solution(relation)
+}
