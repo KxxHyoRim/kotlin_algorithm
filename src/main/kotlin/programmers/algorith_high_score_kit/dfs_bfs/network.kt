5c6a547f966685a0c5247b7681cd834cdec2f5b6
kxxhyorim <rlagyfla0709@gmail.com>
2024-07-04 18:50:22 +0900

    [프로그래머스 고득점 kit] 네트워크 (bfs/dfs)

---
+++ src/main/kotlin/programmers/algorith_high_score_kit/dfs_bfs/network.kt
@@ -0,0 +1,39 @@
+package programmers.algorith_high_score_kit.dfs_bfs
+
+class Solution {
+    var isVisited = mutableListOf<Boolean>()
+    var size = -1
+    lateinit var computers: Array<IntArray>
+    var answer = 0
+
+    fun initIsVisited(size: Int) {
+        isVisited = MutableList(size) { false }
+    }
+
+    fun bfs(idx: Int) {
+        isVisited[idx] = true
+        for (i in 0 until size) {
+            if (i == idx) continue
+            if (!isVisited[i] && computers[idx][i] == 1) {
+                bfs(i)
+            }
+        }
+    }
+
+
+    fun solution(n: Int, computers: Array<IntArray>): Int {
+        size = computers.size
+        this.computers = computers
+
+        initIsVisited(size)
+
+        for (i in 0 until size) {
+            if (!isVisited[i]) {
+                bfs(i)
+                answer++
+            }
+        }
+        println(answer)
+        return answer
+    }
+}
